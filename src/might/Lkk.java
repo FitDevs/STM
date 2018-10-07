d88cc7e1c54b4ee7d92847fccc646538289e7d50
PonPonx2 <PonPonx2@PonPonx2-PC>
2018-10-08 00:34:52 +0900

    hallow world

--- src/might/Lkk.java
+++ src/might/Lkk.java
@@ -4,6 +4,10 @@ public class Lkk {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		StringBuilder a = new StringBuilder();
+		a.append("Hallow World");
+		System.out.println(a);
+
 
 	}
 
